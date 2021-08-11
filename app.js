f567c4b4fce6aba86f2e9493eac4ad1b2c3da502
five minutes <fiveminutes001@gmail.com>
2021-08-11 18:37:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,14 @@ links.forEach(element => {
         console.log("Look, ma! We didn't navigate to a new page!");
 });
 });
+
+const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
+
+for (let i = 0; i < 4; i++) {
+    const newElement = document.createElement('p');
+    newElement.innerText = 'This is paragraph number ' + i;
+
+    fragment.appendChild(newElement);
+}
+
+document.body.appendChild(fragment); // reflow and repaint here -- once!
