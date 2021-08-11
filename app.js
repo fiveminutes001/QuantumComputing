b6ee5ecb15c162ad9a98f97fae5e4a76b065fb2b
five minutes <fiveminutes001@gmail.com>
2021-08-11 11:28:06 +0300

    Update app.js in JsAndTheDOM

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ document.addEventListener('click', function () {
 const links = document.querySelectorAll('a');
 const thirdLink = links[2];
 
-thirdLink.addEventListener('click', function (event) {
-    event.preventDefault();
-    console.log("Look, ma! We didn't navigate to a new page!");
+links.forEach(element => {
+    element.addEventListener('click', function (event) {
+        event.preventDefault();
+        console.log("Look, ma! We didn't navigate to a new page!");
+});
 });
