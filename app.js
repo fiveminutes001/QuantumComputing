fb206cdff4a5bea4f9d39aeb56e67f6e23f2627d
five minutes <fiveminutes001@gmail.com>
2021-08-15 15:34:43 +0300

    Update app.js

--- app.js
+++ app.js
@@ -100,3 +100,16 @@ let cat = {
   }
   
   whoThis();
+
+    
+const returnsAFunction = function () {
+    return function () {
+      console.log('Hello from inside a function');
+    };
+  };
+  
+  const newFunction = returnsAFunction();
+  
+  newFunction();
+  
+  returnsAFunction()();
